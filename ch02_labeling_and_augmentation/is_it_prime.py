4910f40186646944f817200dba4b78d5b86686fc
Kraus, John F CTR (USA) <john.f.kraus19.ctr@mail.mil>
2022-09-09 09:00:33 -0400

    prime number work in progress 2

--- ch02_labeling_and_augmentation/is_it_prime.py
+++ ch02_labeling_and_augmentation/is_it_prime.py
@@ -1,5 +1,7 @@
 from math import sqrt
 import numpy as np
+import re, sys
+import pandas as pd
 
 def is_prime(n):    
     # this flag maintains status whether the n is prime or not
@@ -35,10 +37,93 @@ def prime_factors(n):
 def array_map(x):
     return np.array(list(map(is_prime, x)))
 
+
+
+def is_prime(n):
+    # see http://www.noulakaz.net/weblog/2007/03/18/a-regular-expression-to-check-for-prime-numbers/
+    return re.match(r'^1?$|^(11+?)\1+$', '1' * n) == None
+
+
+def is_prime_int(n):
+    # see http://www.noulakaz.net/weblog/2007/03/18/a-regular-expression-to-check-for-prime-numbers/
+    # print(n, re.match(r'^1?$|^(11+?)\1+$', '1' * n) == None)
+    return 1 if re.match(r'^1?$|^(11+?)\1+$', '1' * n) == None else 0
+
+
+def get_n_primes(n):
+
+    # N = int(sys.argv[1]) # number of primes wanted (from command-line)
+    N = n  # 200  # int(sys.argv[1]) # number of primes wanted (from command-line)
+    M = 100             # upper-bound of search space (we search 100 consecutive integers at a time)
+    l = list()           # result list
+
+    while len(l) < N:    
+        l += filter(is_prime, range(M - 100, M)) # append prime element of [M - 100, M] to l
+        # l += filter(isPrime, range(0, M)) # append prime element of [M - 100, M] to l
+        # if we haven't found N primes yet, we shift the search space up by 100;
+        # i.e., from 0 - 100 to 100 - 200
+        M += 100                                # increment upper-bound
+
+    return l[:N]  # only return the number of primes specified  # print(l)
+
+
+def get_primes_in_first_n_integers(top):
+    l = list()
+    for n in range(0, top):
+        if is_prime(n):
+            l += [n]
+    return l      
+
+
+# if __name__ = "__main__":
+def get_prime_to_integer_ratios():
+    for n in range(100, 1000, 50):
+    #n = 103
+        n_prime_list = get_n_primes(n)
+        npl_ratio = len(n_prime_list)/ n_prime_list[-1]
+        print(n_prime_list[-1], len(n_prime_list), npl_ratio)  #, n_prime_list)
+
+# prime_list_up_to_n = get_primes_in_first_n_integers(100)
+# print('len =', len(prime_list_up_to_n), prime_list_up_to_n)
+
+def make_prime_list(num_primes):
+    outer_prime_list = list()
+    for n in range(0,num_primes):
+        inner_list = None
+        is_prime = is_prime_int(n)
+        if is_prime:
+            inner_list = [n, 1]     
+            # inner_list = inner_list + [1]
+        else:
+            inner_list = [n, 0] 
+            # inner_list = inner_list + [0]
+        outer_prime_list.append(inner_list)
+    return outer_prime_list
+
+
+def make_primes_df(num_primes):
+    prime_list = make_prime_list(num_primes)
+    df = pd.DataFrame(prime_list, columns =['Number', 'ground_truth'])     
+    return df
+
+
+def make_list_of_num_and_labels():
+    nums = list(range(0,200))
+    labels = [0] * 200
+    for num in nums:
+        labels[num] = is_prime_int(num)            
+        # print(num, labels[num])
         
+    return nums, labels
+
+
+
+
 if __name__ == "__main__":
-    for n in range(11):
-        print(n, is_prime(n), prime_factors(n))
-    validation = [22, 11, 7, 2, 32, 101, 102]
-    m = array_map(validation)
-    print(m)
+    # for n in range(11):
+    #     print(n, is_prime(n), prime_factors(n))
+    # validation = [22, 11, 7, 2, 32, 101, 102]
+    # m = array_map(validation)
+    # print(m)
+    # print(make_primes_df(200))
+    make_list_of_num_and_labels()
